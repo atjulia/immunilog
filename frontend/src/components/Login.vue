7888b22b5c497acaddf7990de91800b34430d2d4
julia.karolina <julia.karolina@fulltechpro.com.br>
2024-09-12 20:35:32 -0300

    feat: Iniciando frontend
    Co-authored-by: Júlia Karolina Atanazio <75694566+atjulia@users.noreply.github.com>

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive
+      color="white"
+      class="align-center fill-height mx-auto"
+      max-width="900"
+    >
+      <v-card color="#F8F7F5" class="pa-3">
+        <v-row>
+          <v-col cols="12">
+            <v-img src="@/assets/logo.svg" height="25" />
+          </v-col>
+          <v-col cols="12">
+            <v-text-field label="E-mail" prepend-icon="mdi mdi-email-outline" variant="outlined" />
+          </v-col>
+          <v-col cols="12">
+            <v-text-field label="Senha" prepend-icon="mdi mdi-lock-outline" variant="outlined" />
+          </v-col>
+          <v-col cols="3">
+            <v-btn>Entrar</v-btn>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script setup>
+  //
+</script>
---
+++ frontend/src/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="fill-height">
+    <Login />
+    <!-- <v-toolbar density="compact" color="#F8F7F5">
+      <v-toolbar-title text="immunilog" class="d-flex justify-center"/>
+    </v-toolbar> -->
+  </div>
+</template>
+
+<script setup>
+  //
+</script>
