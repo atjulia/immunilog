dba9e35e149aedf94db6500d07464a6f922d155f
julia.karolina <julia.karolina@fulltechpro.com.br>
2024-10-07 21:29:52 -0300

    feat: Refatoração home part 1
    Co-authored-by: Júlia Karolina Atanazio <75694566+atjulia@users.noreply.github.com>

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <v-row class="px-5 pt-8">
-      <v-col cols="8">
+      <v-col cols="7">
         <v-row>
           <v-col cols="12">
             <span class="text-title">Minha saúde</span>
@@ -34,75 +34,70 @@
             <span class="text-card pt-3">{{ card.text }}</span>
           </v-col>
         </v-row>
-      </v-col>
-      <v-col cols="4">
-        <v-col cols="12" class="px-5">
-          <span class="text-title">Links Úteis</span>
-        </v-col>
         <v-row>
-          <v-col
-            cols="1"
-            class="d-flex flex-column align-items-center pr-4"
-            style="min-width: 125px;"
-          >
-            <div class="d-flex align-items-center justify-center flex-grow-1">
-              <card class="card d-block">
-                link
-              </card>
-            </div>
+          <v-col cols="12" class="px-5">
+            <span class="text-title">Links Úteis</span>
           </v-col>
+          <v-row>
+            <v-col
+              cols="1"
+              class="d-flex flex-column align-items-center pr-4"
+              style="min-width: 125px;"
+            >
+              <div class="d-flex align-items-center justify-center flex-grow-1">
+                <card class="card d-block">
+                  link
+                </card>
+              </div>
+            </v-col>
+          </v-row>
         </v-row>
       </v-col>
-    </v-row>
-    <v-row class="px-5 pt-8">
-      <v-col cols="12" class="px-5">
-        <span class="text-title">Gerenciar Dependentes</span>
-      </v-col>
-      <v-col cols="6" class="mx-5">
-        <v-row class="pb-3">
-          <v-btn density="comfortable" icon color="primary" variant="outlined" @click="addDependente">
-            <v-tooltip
-              activator="parent"
-              location="top"
-            >Adicionar Dependente</v-tooltip>
-            <v-icon color="primary">
+      <v-col cols="5">
+        <v-col cols="12" class="px-5 d-flex justify-space-between">
+          <span class="text-title">Gerenciar Dependentes</span>
+          <v-btn density="comfortable" color="primary" variant="outlined" @click="addDependente">
+            Adicionar Dependente
+            <v-icon color="primary" class="pl-2">
               <PhPlus :size="24" />
             </v-icon>
           </v-btn>
-        </v-row>
-        <v-row>
-          <v-col cols="12" class="pa-0">
-            <v-table
-              style="background-color: #F8F7F5;"
-              density="compact"
-              fixed-header
-            >
-              <thead>
-                <tr>
-                  <th class="text-left">
-                    Nome
-                  </th>
-                  <th class="text-left">
-                    Idade
-                  </th>
-                  <th class="text-left">
-                    Tipo Usuario
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr
-                  v-for="item in pessoas"
-                  :key="item.Id"
-                >
-                  <td>{{ item.Nome }}</td>
-                  <td>{{ item.IdadeFormatada }}</td>
-                  <td>{{ item.TipoPessoa === 1 ? 'Principal' : 'Dependente' }}</td>
-                </tr>
-              </tbody>
-            </v-table>
-          </v-col>
-        </v-row>
+        </v-col>
+        <v-col cols="12" class="mx-5">
+          <v-row v-for="(pessoa, i) in paginatedPessoas" :key="i">
+            <v-col cols="12">
+              <v-card variant="outlined" color="primary" class="mb-4 pa-5">
+                <v-row>
+                  <v-col cols="7">
+                    <div class="text-start">
+                      <strong>{{ pessoa.Nome }}</strong>
+                    </div>
+                    <div class="text-start">
+                      <span class="text-body-2">Idade: {{ pessoa.IdadeFormatada}}</span><br>
+                      <span class="text-body-2">Última imunização: 10/09/2024</span>
+                    </div>
+                  </v-col>
+  
+                  <v-col cols="5" class="d-flex justify-end align-items-center">
+                    <v-chip color="secondary" class="secondary--text text-caption" variant="outlined">
+                      {{ pessoa.TipoPessoa === 1 ? 'Principal' : 'Dependente' }}
+                    </v-chip>
+                    <v-icon color="primary" class="pl-2" style="cursor: pointer;">
+                      <PhDotsThreeVertical :size="32" />
+                    </v-icon>
+                  </v-col>
+                </v-row>
+              </v-card>
+            </v-col>
+          </v-row>
+          <v-pagination
+            v-model="currentPage"
+            :length="totalPages"
+            :total-visible="5"
+            class="mx-auto mt-4"
+            @input="updatePage"
+          />
+       </v-col>
       </v-col>
     </v-row>
     <dependenteEdit ref="dependente" @refresh="fetchPessoas" />
@@ -132,10 +127,20 @@ export default {
   data () {
     return {
       credentials: JSON.parse(localStorage.getItem('credentials')),
-      pessoas: []
+      pessoas: [],
+      currentPage: 1,
+      itemsPerPage: 3
     }
   },
   computed: {
+    paginatedPessoas() {
+      const start = (this.currentPage - 1) * this.itemsPerPage;
+      const end = start + this.itemsPerPage;
+      return this.pessoas.slice(start, end);
+    },
+    totalPages() {
+      return Math.ceil(this.pessoas.length / this.itemsPerPage);
+    },
     cards () {
       return [
         { 
@@ -169,6 +174,9 @@ export default {
     await this.fetchPessoas();
   },
   methods: {
+    updatePage(page) {
+      this.currentPage = page;
+    },
     getImageUrl(imagePath) {
       return new URL(imagePath, import.meta.url).href;
     },
@@ -220,5 +228,16 @@ export default {
   height: 100px;
   border: 2px solid #384593;
   border-radius: 16px;
+}
+.text-body-2 {
+  font-size: 15px;
+}
+strong {
+  font-size: 16px;
+  color: #2f2f8d;
+}
+.v-card {
+  border-color: #2f2f8d;
+  border-radius: 12px;
 }
 </style>
